27f2107d3dbaee80d33796720e9178b1319b1351
Prakritidev Verma <Prakritidevverma@gmail.com>
2019-09-06 13:04:04 +0530

    Developed Authentication APIs and ODATA SF access token generation mechanism. (#7)
    * Feat Admin-User Authentication APIs
    
    *  - [1] feat: added login and signup api
    
    *  - [2] hot-fix: Code Refractoring
    
    * - [3] hot-fix: added admin controller and refractor
    
    * - [4] hot-fix: updated config, added Admin login routes
    
    * - [5] hot-fix: refractor code
    
    * - [6] feat: Admin login and signup, #note: /secret not working
    
    *  - [7] feat: added accessToken in  project structure.
    
    *  - [8] feat: added mongoDB model for registering company
    
    * - [9] hot-fix: removed helper from controller; created helper folder
    
    * - [10] hot-fix: code refractor
    
    *  - [11] hot-fix: updated company model, added helper for odata access token
    
    * -[12] feat: added adminAction
    
    * [13] hot-fix: code refractor
    
    *  - [14] hot-fix: code refractor
    
    *  - [14] feat: updated user login APIs,  added project structure for Odata APIs
    
    * -[15] feat: solved user-admin authentication; added Odata leave management APIs
    
    *  - [16] hot-fix: removed unique company name from user model; updated controllers

---
+++ controller/user/user.auth.controller.js
@@ -0,0 +1,38 @@
+const JWT = require("jsonwebtoken");
+const User = require("../../models/user/auth");
+const { JWT_SECRET } = require("../../config");
+signToken = user => {
+  return JWT.sign(
+    {
+      iss: "Coder",
+      sub: user._id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1)
+    },
+    JWT_SECRET
+  );
+};
+module.exports = {
+  // SIGN UP
+  signup: async (req, res, next) => {
+    const { email, password, companyname, userid } = req.value.body;
+    //check if there is a user with the same email
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      res.status(403).send({ error: "email is already in use" });
+    }
+    const newUser = new User({ email, password, companyname, userid });
+    await newUser.save();
+    const token = signToken(newUser);
+
+    res.status(200).json({ token });
+  },
+
+  // SIGN IN
+  signin: async (req, res, next) => {
+    //console.log('req.user:',req.user);
+    
+
+    res.status(200).json({  Status: "Login Successful" });
+  }
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const config = require("./config/index");
+
+mongoose.connect("mongodb://localhost/mydb", { useNewUrlParser: true });
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// User Routes
+const userRoute = require("./routes/user/auth/userAuth");
+app.use("/user", userRoute);
+
+// Admin Routes
+const adminAuthRoutes = require("./routes/admin/auth/adminAuth");
+app.use("/admin", adminAuthRoutes);
+
+//odata leavemanagement
+const odatamanagement = require("./routes/odata/leaveManagement/getleave");
+app.use("/odata", odatamanagement);
+
+// Admin Actions
+// Admin Routes
+const adminActionsRoutes = require("./routes/admin/actions/adminAction");
+app.use("/admin/action", adminActionsRoutes);
+
+app.listen(config["port"], () => {
+  console.log(`Server listening on port: ${config["port"]}`);
+});
