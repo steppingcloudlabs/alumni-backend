58f791e0160bcc6ce9af857a08ef2ca260c7456f
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-09-19 16:13:07 +0530

    added News and Events APIs (#13) (#14)

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("./config/index");
 
-mongoose.connect("mongodb+srv://root:password@123@dev-i9qmj.mongodb.net/mydb?retryWrites=true&w=majority", { useNewUrlParser: true });
+db = mongoose.connect("mongodb+srv://root:password@123@dev-i9qmj.mongodb.net/mydb?retryWrites=true&w=majority", { useNewUrlParser: true });
 
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-//CORC Headers
+//CORS Headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // User Routes
@@ -30,7 +30,6 @@ app.use("/admin", adminAuthRoutes);
 const odatamanagement = require("./routes/odata/leaveManagement/getleave");
 app.use("/odata", odatamanagement);
 
-// Admin Actions
 // Admin Routes
 const adminActionsRoutes = require("./routes/admin/actions/adminAction");
 app.use("/admin/action", adminActionsRoutes);
@@ -40,5 +39,5 @@ const personalRoutes = require("./routes/user/action/user");
 app.use("/personal", personalRoutes);
 
 app.listen(config["port"], () => {
-  console.log(`Server listening on port: ${config["port"]}`);
+    console.log(`Server listening on port: ${config["port"]}`);
 });
