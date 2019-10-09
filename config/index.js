fb5f3ad96c9303d8a64a90033147df595e5bf656
Maaz Abdullah <maaz.abdullah111@gmail.com>
2019-10-09 11:56:35 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,4 @@
 module.exports = {
   JWT_SECRET: "authentication",
-  port: 4000,
-  accessKeyId: "AKIARKLS7FT44XJEYAFI",
-  secretAccessKey: "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",
-  Bucket : "sclabs-titan"
+  port: 4000
 };
