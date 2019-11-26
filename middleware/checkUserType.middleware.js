49c4fc4947a1c8e05a6616cfe714e0e68eb1afba
maaz1996 <maaz.abdullah111@gmail.com>
2019-11-26 17:50:33 +0530

    -added middleware checks to admin apis

--- middleware/checkUserType.middleware.js
+++ middleware/checkUserType.middleware.js
@@ -9,19 +9,16 @@ module.exports = function(req, res, next) {
   /**
    * Check if user is an admin or not
    */
-  const payload = req.body.type;
-
-  console.log("Middleware is hit");
-
+  const {payload} = req.body;
   try {
-    if (payload == "admin") {
-      //If the type is admin, let them pass
+    if (payload.type == 'admin') {
+      // If the type is admin, let them pass
       next();
     } else {
-      //Send a response that user type is invalid
+      // Send a response that user type is invalid
       res.status(200).send({
         status: 400,
-        result: "Rejected request"
+        result: 'Rejected request',
       });
     }
   } catch (error) {
--- routes/admin/actions/adminaction.js
+++ routes/admin/actions/adminaction.js
@@ -1,6 +1,7 @@
 const router = require('express-promise-router')();
 const passport = require('passport');
 const AdminController = require('../../../controller/admin/actions.admin.controller');
+const checkuserType = require('../../../middleware/checkUserType.middleware');
 require('../../../validator/admin.passport');
 // adminRouter Navigations
 
@@ -13,10 +14,10 @@ router
     .post((req, res, next) => AdminController.viewallNews(req, res, next));
 router
     .route('/updatenews')
-    .post((req, res, next) => AdminController.updateNews(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.updateNews(req, res, next));
 router
     .route('/deletenews')
-    .delete((req, res, next) => AdminController.deleteNews(req, res, next));
+    .delete(checkuserType, (req, res, next) => AdminController.deleteNews(req, res, next));
 router
     .route('/add/event')
     .post((req, res, next) => AdminController.viewEvents(req, res, next));
@@ -26,10 +27,10 @@ router
     .post((req, res, next) => AdminController.viewallEvents(req, res, next));
 router
     .route('/updateevent')
-    .post((req, res, next) => AdminController.updateEvents(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.updateEvents(req, res, next));
 router
     .route('/deleteevent')
-    .delete((req, res, next) => AdminController.deleteEvents(req, res, next));
+    .delete(checkuserType, (req, res, next) => AdminController.deleteEvents(req, res, next));
 
 router
     .route('/add/faq')
@@ -40,31 +41,31 @@ router
     .post((req, res, next) => AdminController.viewallFaq(req, res, next));
 router
     .route('/updatefaq')
-    .post((req, res, next) => AdminController.updatefaq(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.updatefaq(req, res, next));
 router
     .route('/deletefaq')
-    .delete((req, res, next) => AdminController.deleteFaq(req, res, next));
+    .delete(checkuserType, (req, res, next) => AdminController.deleteFaq(req, res, next));
 router
     .route('/alumni')
-    .post((req, res, next) => AdminController.createalumni(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.createalumni(req, res, next));
 router
     .route('/alumniview')
     .post((req, res, next) => AdminController.viewalumni(req, res, next));
 router
     .route('/updatealumni')
-    .post((req, res, next) => AdminController.updatealumni(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.updatealumni(req, res, next));
 router
     .route('/allalumni')
-    .post((req, res, next) => AdminController.allalumni(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.allalumni(req, res, next));
 router
     .route('/deletealumni')
-    .delete((req, res, next) => AdminController.deletealumni(req, res, next));
+    .delete(checkuserType, (req, res, next) => AdminController.deletealumni(req, res, next));
 router
     .route('/userupload')
-    .post((req, res, next) => AdminController.userupload(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.userupload(req, res, next));
 router
     .route('/documentupload')
-    .post((req, res, next) => AdminController.documentupload(req, res, next));
+    .post(checkuserType, (req, res, next) => AdminController.documentupload(req, res, next));
 router
     .route('/documentdownlaod')
     .post((req, res, next) => AdminController.viewdocument(req, res, next));
