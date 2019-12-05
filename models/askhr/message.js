838de023f7bee5f8227232bca9604dfb163ba72c
Prakritidev Verma <prakritidevverma@gmail.com>
2019-12-05 15:12:20 +0530

    feat: added askhr models

---
+++ models/askhr/message.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    senders: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    message: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        unique: true,
+    }
+});
+
+
+const User = mongoose.model('messages', messageSchema);
+module.exports = User;
---
+++ models/askhr/tickets.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ticketSchema = new Schema({
+    participants: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        unique: true,
+    },
+    created_at: {
+        type: Date,
+        required: true,
+    },
+    created_by: {
+        type: Schema.Types.ObjectId,
+    },
+    updated_by: {
+        type: Schema.Types.ObjectId,
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    message: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+    },
+    esclation: {
+        type: Boolean,
+        required: true,
+    },
+    esclation_manager_1: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        response: Boolean,
+    },
+    esclation_manager_2: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        response: Boolean,
+    },
+    esclation_manager_3: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        response: Boolean,
+    },
+    resolved_status: {
+        type: Boolean,
+        required: true,
+        response: Boolean,
+    },
+});
+
+
+const Ticket = mongoose.model('tickets', ticketSchema);
+module.exports = Ticket;
