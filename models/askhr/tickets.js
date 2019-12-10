6442af6a3bf3d46400df39eaf5c90829a0d8316b
maaz1996 <maaz.abdullah111@gmail.com>
2019-12-10 13:05:17 +0530

    updated schema message, tickets

--- models/askhr/tickets.js
+++ models/askhr/tickets.js
@@ -1,71 +1,61 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+// using ref :"user" Schema; both admin and user are present in "user" Schema
 const ticketSchema = new Schema({
-  participants: [
-    {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      ref: 'user',
-    },
-  ],
+  participants: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+
   created_at: {
     type: Date,
     value: Date.now(),
     required: true,
   },
-  created_by: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'user',
-    },
-  ],
-  updated_by: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'user',
-    },
-  ],
+  created_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+
+  updated_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+
   title: {
     type: String,
     required: true,
     unique: true,
   },
-  message: [
-    {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      ref: 'messages',
-    },
-  ],
+  message: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'messages',
+  },
+
   esclation: {
     type: Boolean,
     required: true,
   },
-  esclation_manager_1: [
-    {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      response: Boolean,
-      ref: 'user',
-    },
-  ],
-  esclation_manager_2: [
-    {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      response: Boolean,
-      ref: 'user',
-    },
-  ],
-  esclation_manager_3: [
-    {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      response: Boolean,
-      ref: 'user',
-    },
-  ],
+  esclation_manager_1: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    response: Boolean,
+    ref: 'user',
+  },
+  esclation_manager_2: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    response: Boolean,
+    ref: 'user',
+  },
+  esclation_manager_3: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    response: Boolean,
+    ref: 'user',
+  },
   resolved_status: {
     type: Boolean,
     required: true,
