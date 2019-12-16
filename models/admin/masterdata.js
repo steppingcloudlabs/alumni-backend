a310ab4856cd514ab97bc0a0a9800f6161d7a2aa
maaz1996 <maaz.abdullah111@gmail.com>
2019-12-16 14:10:36 +0530

    updated masterdata; added linkedInlink

--- models/admin/masterdata.js
+++ models/admin/masterdata.js
@@ -1,77 +1,80 @@
-const mongoose = require('mongoose');
-const mongoose_fuzzy_searching = require('mongoose-fuzzy-searching');
-const {each, queue} = require('async');
+const mongoose = require("mongoose");
+const mongoose_fuzzy_searching = require("mongoose-fuzzy-searching");
+const { each, queue } = require("async");
 const Schema = mongoose.Schema;
-require('mongoose-type-url');
+require("mongoose-type-url");
 
 const masterschema = new Schema({
   relieving_date: {
-    type: String,
+    type: String
   },
   user_id: {
     type: String,
-    unique: true,
+    unique: true
   },
   date_of_resignation: {
-    type: String,
+    type: String
   },
   last_working_day_as_per_notice_period: {
-    type: String,
+    type: String
   },
   personal_email_id: {
-    type: String,
+    type: String
   },
   first_name_personal_information: {
-    type: String,
+    type: String
   },
   last_name_personal_information: {
-    type: String,
+    type: String
   },
   middle_name_personal_information: {
-    type: String,
+    type: String
   },
   nationality_personal_information: {
-    type: String,
+    type: String
   },
   salutation_personal_information: {
-    type: String,
+    type: String
   },
   city_addresses: {
-    type: String,
+    type: String
   },
   phone_number_phone_information: {
-    type: String,
+    type: String
   },
   manager_job_information: {
-    type: String,
+    type: String
   },
   designation_job_information: {
-    type: String,
+    type: String
   },
   gender: {
-    type: String,
+    type: String
   },
   date_of_birth: {
-    type: String,
+    type: String
   },
   skill: [
     {
-      type: String,
-    },
+      type: String
+    }
   ],
+  linkedInlink: {
+    type: String
+  }
 });
 
 // finding mongoose fuzzy search based fields in masterdata
 masterschema.plugin(mongoose_fuzzy_searching, {
   fields: [
-    'first_name_personal_information',
-    'personal_email_id',
-    'user_id',
-    'middle_name_personal_information',
-  ],
+    "first_name_personal_information",
+    "personal_email_id",
+    "user_id",
+    "middle_name_personal_information"
+  ]
 });
 // removing anagrams from existing fields from masterschema in order for partial search
-const masterdata = mongoose.model('masterdata', masterschema);
+const masterdata = mongoose.model("masterdata", masterschema);
 const updateFuzzy = async (Model, attrs) => {
   const docs = await Model.find();
 
@@ -79,8 +82,8 @@ const updateFuzzy = async (Model, attrs) => {
     try {
       if (attrs && attrs.length) {
         const obj = attrs.reduce(
-            (acc, attr) => ({...acc, [attr]: data[attr]}),
-            {}
+          (acc, attr) => ({ ...acc, [attr]: data[attr] }),
+          {}
         );
         return Model.findByIdAndUpdate(data._id, obj).exec();
       }
@@ -92,13 +95,13 @@ const updateFuzzy = async (Model, attrs) => {
     }
   };
   const myQueue = queue(updateToDatabase, 10);
-  each(docs, (data) => myQueue.push(data.toObject()));
+  each(docs, data => myQueue.push(data.toObject()));
   myQueue.empty = function() {};
   myQueue.drain = function() {};
 };
 // usage
-updateFuzzy(masterdata, ['first_name_personal_information']);
-updateFuzzy(masterdata, ['personal_email_id']);
-updateFuzzy(masterdata, ['user_id']);
-updateFuzzy(masterdata, ['middle_name_personal_information']);
+updateFuzzy(masterdata, ["first_name_personal_information"]);
+updateFuzzy(masterdata, ["personal_email_id"]);
+updateFuzzy(masterdata, ["user_id"]);
+updateFuzzy(masterdata, ["middle_name_personal_information"]);
 module.exports = masterdata;
