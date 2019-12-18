53a31440e035f85ccac740a72ffb23874dc29322
Prakritidev Verma <prakritidevverma@gmail.com>
2019-12-18 11:24:42 +0530

    updated manager schema

--- models/askhr/managers.js
+++ models/askhr/managers.js
@@ -2,19 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // using ref :"user" Schema; both admin and user are present in "user" Schema
 const managerSchema = new Schema({
-  esclation_manager_1: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  },
+    esclation_manager_1: {
+        manager_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        level: {
+            type: String,
+            enum: ["esclation_manager_1"],
+            default: "esclation_manager_1"
+        }
 
-  esclation_manager_2: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  },
-  esclation_manager_3: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  },
+    },
+    esclation_manager_2: {
+        manager_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        level: {
+            type: String,
+            enum: ["esclation_manager_2"],
+            default: "esclation_manager_2"
+        }
+    },
+    esclation_manager_3: {
+        manager_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        level: {
+            type: String,
+            enum: ["esclation_manager_3"],
+            default: "esclation_manager_3"
+        }
+    },
 });
 
 const manager = mongoose.model('managers', managerSchema);
