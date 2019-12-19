500390c8a143264d9fbef51f4cc94d2ccdd8f70a
maaz1996 <maaz.abdullah111@gmail.com>
2019-12-19 13:01:01 +0530

    hot-fix; comments added

--- utils/getFirstMessageDateFromTicket.js
+++ utils/getFirstMessageDateFromTicket.js
@@ -1,29 +1,32 @@
-const Ticket = require('../models/askhr/tickets');
+const Ticket = require("../models/askhr/tickets");
 
 module.exports = () => {
-  const getFirstMessageDateFromTicket = (_id) =>
+  const getFirstMessageDateFromTicket = _id =>
     new Promise(async (resolve, reject) => {
       try {
-        const result = await Ticket.findOne({_id}).populate({
-          path: 'message',
-          select: {_id: 0, senders: 1, message: 1, created_at: 1},
+        //finding the ticket; populate messagebody and senders in message; sort in decreasing order of message exchanged between admin and user.
+        const result = await Ticket.findOne({ _id }).populate({
+          path: "message",
+          select: { _id: 0, senders: 1, message: 1, created_at: 1 },
           options: {
-            sort: {created_at: -1},
+            sort: { created_at: -1 }
           },
-          populate: {path: 'senders', select: {_id: 0, userType: 1}},
+          populate: { path: "senders", select: { _id: 0, userType: 1 } }
         });
-
+        // finding the first admin message after navigation; the next message will be message of user who is querying to escalate;
+        //the date of last user message will be his query for escalation;  escalation date will be counted from that date.
         let firstUserMessage;
         for (let i = 0; i < result.message.length; i++) {
-          if (result.message[i].senders.userType == 'admin') {
+          if (result.message[i].senders.userType == "admin") {
             firstUserMessage = result.message[i - 1];
             break;
           }
         }
         // const newtime=firstUserMessage.created_at+new Date().getTime(new Date().setTime() + 900000 );
         // const okay= newtime.toString();
-        const escalationDate =
-        firstUserMessage.created_at.setDate(firstUserMessage.created_at.getDate() + 1 );
+        const escalationDate = firstUserMessage.created_at.setDate(
+          firstUserMessage.created_at.getDate() + 1
+        );
 
         resolve(escalationDate);
       } catch (error) {
@@ -31,6 +34,6 @@ module.exports = () => {
       }
     });
   return {
-    getFirstMessageDateFromTicket,
+    getFirstMessageDateFromTicket
   };
 };
