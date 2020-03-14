ad8589fb01476bc64ec078832767868ceb439e01
Rahil Khan <rahil4rahil@gmail.com>
2020-03-14 21:33:44 +0530

    header added

--- controller/orgchart/orgchart.controller.js
+++ controller/orgchart/orgchart.controller.js
@@ -19,7 +19,13 @@ module.exports = () => {
         })
         if (response) {
           let formattedData = formatSapData(response)
-          res.setHeader("Content-Type", "application/json")
+          res.header("Content-Type", "application/json")
+          res.header("Access-Control-Allow-Origin", "*")
+          res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE,");
+          res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+          );
           res.status(200).send(formattedData);
         }
       }
